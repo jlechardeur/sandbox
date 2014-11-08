976e7ce52013a06507e3673f92da5306293a960f
jlechardeur <jlechardeur@gmail.com>
2014-11-08 21:29:59 +0100

    webappRest
    creation

---
+++ com.jle.webapp/src/main/java/com/jle/webapp/rest/services/SimpleRestService.java
@@ -0,0 +1,29 @@
+package com.jle.webapp.rest.services;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jle.webapp.rest.pojo.Dummy;
+
+@Controller
+public class SimpleRestService {
+	
+	@RequestMapping(value = "/services/dummy", method = RequestMethod.GET)
+    public @ResponseBody Dummy getDummy() {
+        Dummy dummy = new Dummy();
+        dummy.setId(1);
+        dummy.setName("dummy1");
+        return dummy;
+    }
+	
+	@RequestMapping(value = "/services/dummy/{id}", method = RequestMethod.GET)
+    public @ResponseBody Dummy getDummy(@PathVariable("id") int id) {
+        Dummy dummy = new Dummy();
+        dummy.setId(1);
+        dummy.setName("dummyGET-"+id);
+        return dummy;
+    }
+}
